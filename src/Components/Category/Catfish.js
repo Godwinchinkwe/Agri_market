b00258bec46ca924f12027166094d89b23701325
Your Name <chinkwegodwin@gmail.com>
2023-03-06 10:02:06 +0100

    Changes

--- src/Components/Category/Catfish.js
+++ src/Components/Category/Catfish.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom"
-import './Chicken.css'
+import './Catfish.css'
 import Loading from '../Loading/Loading';
+import Footer from "../Landing/Footer/Footer"
 
 function Catfish() {
 
@@ -25,14 +26,15 @@ function Catfish() {
 
 
   return (
-    <div className='Category-Holder'>
-    <div className='Category-Item-Holder'>
+    <>
+    <div className='Category-Holder1'>
+    <div className='Category-Item-Holder1'>
       {load? <Loading /> :catigory?.map((item) => (
-        <Link key={item.id} className='Category-Place-holder' to={`/detail/${item.id}`} >
-          <div className='Category-Image-holder'>
-            <img src={item.image} alt="" className='Category-Image' />
+        <Link key={item.id} className='Category-Place-holder1' to={`/detail/${item.id}`} >
+          <div className='Category-Image-holder1'>
+            <img src={item.image} alt="" className='Category-Image1' />
           </div>
-          <div className='Category-Details'>
+          <div className='Category-Details1'>
             <p>{item.productName}</p>
             <h4> Price: ₦ {item.price}</h4>
             <h4>Quantity: {item.qty}</h4>
@@ -41,6 +43,8 @@ function Catfish() {
       ))}
     </div>
   </div>
+  <Footer />
+  </>
   )
 }
 
--- src/Components/Category/Chicken.js
+++ src/Components/Category/Chicken.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 import './Chicken.css'
 import Loading from '../Loading/Loading';
+import Footer from '../Landing/Footer/Footer';
+import { useDispatch } from 'react-redux';
+import {useNavigate} from "react-router-dom"
+import { BsCart4 } from "react-icons/bs";
+import Swal from 'sweetalert2'
+import { addToCart, total } from "../../Redux/Features";
+// import './Goods.css'
+// import Footer from '../Landing/Footer/Footer';
 
 
 const Chicken = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const [catigory, setCategory] = useState()
   
 const [load, setLoad] = useState(false)
@@ -25,10 +35,11 @@ const [load, setLoad] = useState(false)
   
 
   return (
+    <>
     <div className='Category-Holder'>
       <div className='Category-Item-Holder'>
         {load? <Loading /> :catigory?.map((item) => (
-          <Link key={item.id} className='Category-Place-holder' to={`/detail/${item.id}`} >
+          <div key={item.id} className='Category-Place-holder'  >
             <div className='Category-Image-holder'>
               <img src={item.image} alt="" className='Category-Image' />
             </div>
@@ -36,11 +47,35 @@ const [load, setLoad] = useState(false)
               <p>{item.productName}</p>
               <h4> Price: ₦ {item.price}</h4>
               <h4>Quantity: {item.qty}</h4>
-            </div>
-          </Link>
+            {/* </div> */}
+            <div className='Goods_decision'>
+    <button className='goods_cart' 
+     onClick={()=>{dispatch(addToCart(item)); dispatch(total());
+      const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true, 
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+Toast.fire({
+  icon: 'success',
+  title: 'Item added successfully'
+})
+      }}> Add to <BsCart4/> </button>
+    <button className='goods_details' onClick={()=> navigate(`/Detail/${item._id}`)} >Details</button>  
+    </div>
+    </div>     
+      </div>
         ))}
       </div>
     </div>
+    <Footer />
+    </>
   )
 }
 export default Chicken
